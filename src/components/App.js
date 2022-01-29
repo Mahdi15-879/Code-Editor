42eb969866e1db4cf06092ef4343c071727fcd8d
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2022-01-29 14:47:03 +0330

    Add fontawesome library, create custom hook and finish project

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 import Editor from "./Editor";
 
 function App() {
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `);
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
   return (
     <>
       <div className="pane top-pane">
-        <Editor />
-        <Editor />
-        <Editor />
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
       </div>
       <div className="pane">
         <iframe
+          srcDoc={srcDoc}
           title="output"
           sandbox="allow-scripts"
           frameBorder="0"
