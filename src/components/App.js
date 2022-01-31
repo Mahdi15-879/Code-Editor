cadeb66274b257c5b6f4ba875b50eda05ed47aef
Mahdi-Heydari <mahdiheydari15879@gmail.com>
2022-01-31 21:26:48 +0330

    Solve problems

--- src/components/App.js
+++ src/components/App.js
@@ -53,18 +53,56 @@ function App() {
     );
   };
 
+  const input = document.querySelector('input[type="file"]');
+  const inputChangeHandler = (e) => {
+    let files = input.files;
+    if (files.length == 0) return;
+    const file = files[0];
+
+    let reader = new FileReader();
+
+    if (file.name.includes(".html")) {
+      reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        setHtml(lines.join("\n"));
+      };
+    } else if (file.name.includes(".css")) {
+      reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        setCss(lines.join("\n"));
+      };
+    } else if (file.name.includes(".js")) {
+      reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split(/\r\n|\n/);
+        setJs(lines.join("\n"));
+      };
+    }
+
+    reader.onerror = (e) => alert(e.target.error.name);
+    reader.readAsText(file);
+  };
+
   return (
     <div className="App">
-      <div className="col-left">
-        <h1>Online Code Editor</h1>
-        <form onSubmit={formHandler}>
-          <input type="file" name="file" accept=".html, .css, .js" />
-          <button type="submit">Upload</button>
-        </form>
-        <h3>Uploaded {progress} %</h3>
-      </div>
-      <div className="col-right">
-        <div className="pane top-pane">
+      <div v className="row-top">
+        <div className="col-left">
+          <h1>Online Code Editor</h1>
+          <form onSubmit={formHandler}>
+            <input
+              type="file"
+              name="file"
+              accept=".html, .css, .js"
+              id="input"
+              onChange={inputChangeHandler}
+            />
+            <button type="submit">Upload</button>
+          </form>
+          <h3>Uploaded {progress} %</h3>
+        </div>
+        <div className="pane top-pane col-right">
           <Editor
             language="xml"
             displayName="HTML"
@@ -87,6 +125,8 @@ function App() {
             fileType="js"
           />
         </div>
+      </div>
+      <div className="row-down">
         <div className="pane">
           <iframe
             srcDoc={srcDoc}
